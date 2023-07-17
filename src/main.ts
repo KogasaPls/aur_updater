079ce37317eb9712fa580491a61e164cee390542
Kogasa <kogasapls@gmail.com>
2023-07-16 20:51:03 -0500

    ???????

--- src/main.ts
+++ src/main.ts
@@ -24,9 +24,9 @@ async function run(): Promise<void> {
 }
 
 function get_update_script(): UpdateScript {
-  const repo: string = core.getInput('aur-package', {required: true})
+  const repo: string = core.getInput('pkgname', {required: true})
   core.exportVariable('aur-package', repo)
-  core.info(`::export-variable name=aur-package::${repo}`)
+  core.info(`pkgname: ${repo}`)
 
   switch (repo) {
     case 'wine-ge-lutris-bin':
